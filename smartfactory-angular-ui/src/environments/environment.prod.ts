fe1cc4b5c8aef3c2fc0c36f16a4a0e4bb6eed9fb
Dragomir Alin <dragomirdanielalin@gmail.com>
2024-04-13 14:06:24 +0300

    remove ui

--- smartfactory-angular-ui/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  authUrl: "https://id.dragomiralin.ro/auth/",
-  realm: "smartfactory",
-  clientId: "jwtClient",
-  api: "https://api.dragomiralin.ro/api/v1/"
-};
